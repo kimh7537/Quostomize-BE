77435948447bff0335ed87d8a07a398933c56a55
ChanUi <hcu55@naver.com>
2024-11-04 15:56:55 +0900

    first commit

---
+++ src/test/java/com/quostomize/quostomize_be/QuostomizeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quostomize.quostomize_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuostomizeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
