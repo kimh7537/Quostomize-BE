cfbee47cce32082a7aec0497e42512179db8d585
kimh7537 <kimh7537@gmail.com>
2024-11-04 17:28:57 +0900

    Feat/domain레포지토리 추가

---
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/card/entity/Card.java
@@ -0,0 +1,16 @@
+package com.quostomize.quostomize_be.domain.customizer.card.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
