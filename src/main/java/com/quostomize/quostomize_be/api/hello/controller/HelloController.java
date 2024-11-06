456f02501fd72f4399627798b8a215abd6b52203
kimh7537 <kimh7537@gmail.com>
2024-11-06 12:25:48 +0900

    Feat/querydsl: querydsl관련 의존성 추가

--- src/main/java/com/quostomize/quostomize_be/api/hello/controller/HelloController.java
+++ src/main/java/com/quostomize/quostomize_be/api/hello/controller/HelloController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
     @GetMapping("/hello")
     @Operation(summary = "유저 프로필 이미지 삭제", description = "유저의 프로필 이미지를 삭제하고 기본 이미지로 설정합니다.")
     public String hello() {
+
         throw new EntityNotFoundException();
+
 //        return "hello";
     }
 }
