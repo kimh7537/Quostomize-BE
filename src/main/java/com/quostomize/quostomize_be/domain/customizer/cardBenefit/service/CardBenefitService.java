6022fafe123b0dbddf76285c9237da0632fe7cdf
seonmin5 <seonmin5555@gmail.com>
2024-11-07 16:38:49 +0900

    Feat/viewCardBenefits: 로그인한 고객의 카드 혜택 조회 기능 추가

---
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/cardBenefit/service/CardBenefitService.java
@@ -0,0 +1,28 @@
+package com.quostomize.quostomize_be.domain.customizer.cardBenefit.service;
+
+import com.quostomize.quostomize_be.api.cardBenefit.dto.CardBenefitResponse;
+import com.quostomize.quostomize_be.domain.customizer.cardBenefit.entity.CardBenefit;
+import com.quostomize.quostomize_be.domain.customizer.cardBenefit.repository.CardBenefitRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class CardBenefitService {
+    private final CardBenefitRepository cardBenefitRepository;
+
+    public CardBenefitService(CardBenefitRepository cardBenefitRepository) {
+        this.cardBenefitRepository = cardBenefitRepository;
+    }
+
+    public List<CardBenefitResponse> findAll() {
+        // TODO: 하드코딩된 customer 정보 변경 필요
+        Long cardId = 1L;
+        Set<CardBenefit> cardBenefits = cardBenefitRepository.findCardBenefitsByCardCardId(cardId);
+        return cardBenefits.stream().map(CardBenefitResponse::from).collect(Collectors.toList());
+    }
+}
