c938cc32ef700fba39f19a7597caa60bc510e811
newgamer11 <qfpotw4u81@naver.com>
2024-11-09 17:56:10 +0900

    Refactor/Stock : StcokInterestDto, StockInterestRepository, StockInterestService - 위시리스트 조회시 presigned url 조회 기능 추가

--- src/main/java/com/quostomize/quostomize_be/api/hello/dto/StockInterestDto.java
+++ src/main/java/com/quostomize/quostomize_be/api/hello/dto/StockInterestDto.java
@@ -3,6 +3,10 @@ package com.quostomize.quostomize_be.api.hello.dto;
 public record StockInterestDto (
         Integer priority,
         String stockName,
-        Integer stockPresentPrice
+        Integer stockPresentPrice,
+        String stockImage
 ) {
+    public StockInterestDto withStockImage(String newStockImage) {
+        return new StockInterestDto(priority, stockName, stockPresentPrice, newStockImage);
+    }
 }
--- src/main/java/com/quostomize/quostomize_be/domain/customizer/stock/service/StockInterestService.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/stock/service/StockInterestService.java
@@ -1,23 +1,37 @@
 package com.quostomize.quostomize_be.domain.customizer.stock.service;
 
+import com.quostomize.quostomize_be.common.s3.S3Service;
 import com.quostomize.quostomize_be.domain.customizer.stock.repository.StockInterestRepository;
 import com.quostomize.quostomize_be.api.hello.dto.StockInterestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StockInterestService {
 
-
+    @Autowired
     private final StockInterestRepository stockInterestRepository;
+
+    @Autowired
+    private final S3Service s3Service;
 
     // 위시리스트를 조회합니다.
     public List<StockInterestDto> getStockWishList() {
         List<StockInterestDto> allStockInterestDto = stockInterestRepository.findAllStockInterestDto();
-        return allStockInterestDto;
+        List<StockInterestDto> collect = allStockInterestDto.stream()
+                .map(stock -> {
+                    // stockImage로 presigned URL을 생성
+                    String preSignedUrl = s3Service.getPreSignedUrl(stock.stockImage());
+                    // stock DTO를 수정하여 새로운 객체를 반환
+                    return stock.withStockImage(preSignedUrl); // 새로운 객체 반환
+                })
+                .collect(Collectors.toList());
+        return collect;
     }
 
     // 위시리스트 중 해당 id에 해당하는 특정항목을 제거합니다.
