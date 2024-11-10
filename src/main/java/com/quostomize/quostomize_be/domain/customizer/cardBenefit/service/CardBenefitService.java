c1c22ac4b4a86fc9231fe2366804e74d7acaf899
seonmin5 <seonmin5555@gmail.com>
2024-11-11 03:58:11 +0900

    Feat/getBenefitChangeDate: 혜택 변경 가능일자 계산 및 그에 따른 버튼 내용 Service 생성

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/cardBenefit/service/CardBenefitService.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/cardBenefit/service/CardBenefitService.java
@@ -1,13 +1,13 @@
 package com.quostomize.quostomize_be.domain.customizer.cardBenefit.service;
 
-import com.quostomize.quostomize_be.api.cardBenefit.dto.CardBenefitRequest;
 import com.quostomize.quostomize_be.api.cardBenefit.dto.CardBenefitResponse;
-import com.quostomize.quostomize_be.domain.customizer.benefit.entity.BenefitCommonCode;
 import com.quostomize.quostomize_be.domain.customizer.cardBenefit.entity.CardBenefit;
 import com.quostomize.quostomize_be.domain.customizer.cardBenefit.repository.CardBenefitRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,40 +15,35 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 public class CardBenefitService {
+    LocalDateTime recentTime = LocalDateTime.now();
+
     private final CardBenefitRepository cardBenefitRepository;
 
     public CardBenefitService(CardBenefitRepository cardBenefitRepository) {
         this.cardBenefitRepository = cardBenefitRepository;
     }
-
+    
+    // 혜택 내역 조회
     public List<CardBenefitResponse> findAll() {
         // TODO: 하드코딩된 customer 정보 변경 필요
         Long cardId = 2L;
         boolean isActive = true;
-        Set<CardBenefit> cardBenefits = cardBenefitRepository.findCardBenefitsByCardCardIdAndIsActive(cardId, isActive);
+        Set<CardBenefit> cardBenefits = cardBenefitRepository.findCardBenefitsByCardDetailCardSequenceIdAndIsActive(cardId, isActive);
         return cardBenefits.stream().map(CardBenefitResponse::from).collect(Collectors.toList());
     }
 
-    public void updateCardBenefits(List<CardBenefitRequest> cardBenefitRequests) {
-        // cardId와 isActive로 CardBenefit 목록 검색
-        Set<CardBenefit> cardBenefits;
-        for (CardBenefitRequest request : cardBenefitRequests) {
-            cardBenefits = cardBenefitRepository.findCardBenefitsByCardCardIdAndIsActive(request.cardId(), request.isActive());
-
-            // 해당 benefitId에 맞는 CardBenefit을 찾고 업데이트
-            cardBenefits.stream()
-                    .filter(cb -> cb.getBenefitId().equals(request.benefitId()))
-                    .findFirst()
-                    .ifPresent(cardBenefit -> {
-                        cardBenefitRepository.save(
-                                cardBenefit.builder()
-                                        .benefitRate(request.benefitRate())
-                                        .isActive(request.isActive())
-                                        .upperCategory(BenefitCommonCode.builder().benefitCommonId(request.upperCategoryId()).build())
-                                        .lowerCategory(BenefitCommonCode.builder().benefitCommonId(request.lowerCategoryId()).build())
-                                        .build()
-                        );
-                    });
-        }
+    // 혜택 변경 가능일자 계산
+    public Boolean getBenefitChangeDate(CardBenefit cardBenefit) {
+        LocalDateTime createdAt = cardBenefit.getCreatedAt();
+        long daysDifference = ChronoUnit.DAYS.between(createdAt, recentTime);
+        return daysDifference >= 30;
     }
+    
+    // 변경 일자에 따른 버튼 내용
+    public String getBenefitChangeButtonLabel(CardBenefit cardBenefit) {
+        LocalDateTime createdAt = cardBenefit.getCreatedAt();
+        long daysDifference = ChronoUnit.DAYS.between(createdAt, recentTime);
+        return daysDifference >= 30 ? "변경하기" : "예약하기";
+    }
+
 }
