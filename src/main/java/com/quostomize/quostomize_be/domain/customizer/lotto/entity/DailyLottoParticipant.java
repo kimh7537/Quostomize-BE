74ea886aa7e439f71a19e0d705be2a450323dde6
ChanUi <hcu55@naver.com>
2024-11-11 17:13:50 +0900

    Feat/lottoParticipant: DailyLottoParticipant @Builder 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/lotto/entity/DailyLottoParticipant.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/lotto/entity/DailyLottoParticipant.java
@@ -3,12 +3,16 @@ package com.quostomize.quostomize_be.domain.customizer.lotto.entity;
 import com.quostomize.quostomize_be.common.entity.BaseTimeEntity;
 import com.quostomize.quostomize_be.domain.customizer.customer.entity.Customer;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "daily_lotto_participant")
 public class DailyLottoParticipant extends BaseTimeEntity {
 
