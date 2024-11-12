3660fc4dad564057ded9a088d4503ec92d96247e
seonmin5 <seonmin5555@gmail.com>
2024-11-12 16:54:55 +0900

    Refactor/inquiryRegistration: DTO 타입 통일

---
+++ src/main/java/com/quostomize/quostomize_be/common/dto/ResponseDTO.java
@@ -0,0 +1,12 @@
+package com.quostomize.quostomize_be.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDTO<T> {
+
+    private T data;
+
+}
