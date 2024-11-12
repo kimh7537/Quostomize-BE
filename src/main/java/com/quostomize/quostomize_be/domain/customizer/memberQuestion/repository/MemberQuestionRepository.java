ae4ce2af424a87c4c95016764ba2b5ba1a7e8cfe
seonmin5 <seonmin5555@gmail.com>
2024-11-12 19:28:45 +0900

    Feat/inquiryListSearch: 페이지 조회 Reposiroty 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/memberQuestion/repository/MemberQuestionRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/memberQuestion/repository/MemberQuestionRepository.java
@@ -1,7 +1,10 @@
-package com.quostomize.quostomize_be.memberQuestion.repository;
+package com.quostomize.quostomize_be.domain.customizer.memberQuestion.repository;
 
-import com.quostomize.quostomize_be.memberQuestion.entity.MemberQuestion;
+import com.quostomize.quostomize_be.domain.customizer.memberQuestion.entity.MemberQuestion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberQuestionRepository extends JpaRepository<MemberQuestion, Integer> {
+    Page<MemberQuestion> findAll(Pageable pageable);
 }
