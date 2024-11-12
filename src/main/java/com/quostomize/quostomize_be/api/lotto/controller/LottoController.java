31da4145c0e74645dd1bd1a7643e9d4370d177e8
ChanUi <hcu55@naver.com>
2024-11-12 15:36:50 +0900

    Feat/lottoController: LottoController ReponseEntity 및 ResponseDTO 변경

--- src/main/java/com/quostomize/quostomize_be/api/lotto/controller/LottoController.java
+++ src/main/java/com/quostomize/quostomize_be/api/lotto/controller/LottoController.java
@@ -2,8 +2,10 @@ package com.quostomize.quostomize_be.api.lotto.controller;
 
 import com.quostomize.quostomize_be.api.lotto.dto.LottoParticipantRequestDto;
 import com.quostomize.quostomize_be.api.lotto.dto.LottoParticipantResponseDto;
+import com.quostomize.quostomize_be.common.dto.ResponseDTO;
 import com.quostomize.quostomize_be.domain.customizer.lotto.service.LottoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +18,20 @@ public class LottoController {
     private final LottoService lottoService;
 
     @PostMapping("/lottery")
-    public List<LottoParticipantResponseDto> registerLottoParticipants() {
-        return lottoService.registerLottoParticipants();
+    public ResponseEntity<ResponseDTO<List<LottoParticipantResponseDto>>> registerLottoParticipants() {
+        List<LottoParticipantResponseDto> participantResponseDtos = lottoService.registerLottoParticipants();
+        return ResponseEntity.ok(new ResponseDTO<>(participantResponseDtos));
     }
 
     @PostMapping("/my-card/change-lotto")
-    public void toggleLottoParticipation(@RequestBody LottoParticipantRequestDto request) {
+    public ResponseEntity<String> toggleLottoParticipation(@RequestBody LottoParticipantRequestDto request) {
         lottoService.toggleLottoParticipation(request);
+        return ResponseEntity.ok("로또 참여 설정이 변경되었습니다.");
     }
 
     @GetMapping("/lottery")
-    public Long getTotalLottoParticipantsCount() {
-        return lottoService.getTotalLottoParticipantsCount();
+    public ResponseEntity<Long> getTotalLottoParticipantsCount() {
+        Long totalParticipants = lottoService.getTotalLottoParticipantsCount();
+        return ResponseEntity.ok(totalParticipants);
     }
 }
