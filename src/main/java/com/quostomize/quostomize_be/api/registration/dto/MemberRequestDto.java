05748eb2ab0d0cbf2c4f223adadb77971f80ce8a
bangsk <cats7858@gmail.com>
2024-11-13 15:50:23 +0900

    requestDto, responseDto refactoring 및  service, controller 일부 수정

--- src/main/java/com/quostomize/quostomize_be/api/registration/dto/MemberRequestDto.java
+++ src/main/java/com/quostomize/quostomize_be/api/registration/dto/MemberRequestDto.java
@@ -1,28 +1,66 @@
 package com.quostomize.quostomize_be.api.registration.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.Length;
+import com.quostomize.quostomize_be.domain.customizer.registration.entity.Member;
 
 public record MemberRequestDto(
-        @NotBlank(message = "이름은 필수 입력 값입니다.") String memberName,
-        @NotBlank(message = "이메일은 필수 입력 값입니다.") @Email(message = "이메일 형식으로 입력해주세요.") String memberEmail,
-        @NotBlank String memberLoginId,
-        @NotBlank @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.") String memberPassword,
-        @NotBlank @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.") String memberPasswordConfirm,
-        @NotBlank String residenceNumber,
-        @NotBlank String zipCode,
-        @NotBlank String memberAddress,
-        @NotBlank(message = "상세주소를 입력하세요.") String memberDetailAddress,
-        @NotBlank String memberPhoneNumber,
-        @NotBlank @Length(min = 6, max = 6, message = "2차 인증번호 6자리를 입력해주세요.") String secondaryAuthCode,
-        @NotBlank @Length(min = 6, max = 6, message = "2차 인증번호 6자리를 입력해주세요.") String secondaryAuthCodeConfirm
-) {}
+        String memberName,
+        String memberEmail,
+        String memberLoginId,
+        String memberPassword,
+        String memberPasswordConfirm,
+        String residenceNumber,
+        String zipCode,
+        String memberAddress,
+        String memberDetailAddress,
+        String memberPhoneNumber,
+        String secondaryAuthCode,
+        String secondaryAuthCodeConfirm
+) {
+    public static MemberRequestDto from(Member member) {
+        return new MemberRequestDto(
+                member.getMemberName(),
+                member.getMemberEmail(),
+                member.getMemberLoginId(),
+                member.getMemberPassword(),
+                null,
+                member.getResidenceNumber(),
+                member.getZipCode(),
+                member.getMemberAddress(),
+                member.getMemberDetailAddress(),
+                member.getMemberPhoneNumber(),
+                member.getSecondaryAuthCode(),
+                null
+        );
+    }
 
+    public Member toEntity() {
+        return Member.builder()
+                .memberName(this.memberName())
+                .memberEmail(this.memberEmail())
+                .memberLoginId(this.memberLoginId())
+                .memberPassword(this.memberPassword())
+                .residenceNumber(this.residenceNumber())
+                .zipCode(this.zipCode())
+                .memberAddress(this.memberAddress())
+                .memberDetailAddress(this.memberDetailAddress())
+                .memberPhoneNumber(this.memberPhoneNumber())
+                .secondaryAuthCode(this.secondaryAuthCode())
+                .build();
+    }
 
-// 관리자 추가하면 필요할 수도 있음.
-//    public enum Role {
-//        USER, ADMIN
-//    }
-//    @Enumerated(EnumType.STRING)
-//    private Role role;//
+    public boolean isValidPassword() {
+        return this.memberPassword().equals(this.memberPasswordConfirm());
+    }
+
+    public boolean isValidSecondaryAuthCode() {
+        return this.secondaryAuthCode().equals(this.secondaryAuthCodeConfirm());
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        return phoneNumber != null && phoneNumber.matches("^\\d{10,11}$");
+    }
+
+    public static boolean isValidEmail(String email) {
+        return email != null && email.matches("^[A-Za-z0-9+_.-]+@(.+)$");
+    }
+}
