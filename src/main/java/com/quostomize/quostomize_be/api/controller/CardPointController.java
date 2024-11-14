07dd2d269ef8b76cdd136d2b150db140fbaae4b2
newgamer11 <qfpotw4u81@naver.com>
2024-11-14 11:46:20 +0900

    Refactor/CardPoint: CardPointController - 스웨거 추가
    @Operation 어노테이션 추가

--- src/main/java/com/quostomize/quostomize_be/api/controller/CardPointController.java
+++ src/main/java/com/quostomize/quostomize_be/api/controller/CardPointController.java
@@ -2,6 +2,13 @@ package com.quostomize.quostomize_be.api.controller;
 
 import com.quostomize.quostomize_be.domain.customizer.point.entity.CardPoint;
 import com.quostomize.quostomize_be.domain.customizer.point.service.CardPointService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,6 +21,7 @@ public class CardPointController {
 
     private final CardPointService cardPointService;
 
+    @Operation(summary = "카드 포인트 생성하기", description = "연결된 카드에 카드 포인트를 생성합니다.")
     @PostMapping("/api/point")
     public ResponseEntity<CardPoint> createCardPoint(@RequestParam Long cardId){
         CardPoint cardPoint = cardPointService.createCardPoint(cardId);
