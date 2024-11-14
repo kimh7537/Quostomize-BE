fc30e5ccce35af3b6e0b1b0509683e7a7586dd84
seonmin5 <seonmin5555@gmail.com>
2024-11-14 14:59:19 +0900

    Refactor/validationErrorHandling: 카드 및 카드 혜택 ErrorCode 추가

--- src/main/java/com/quostomize/quostomize_be/common/error/ErrorCode.java
+++ src/main/java/com/quostomize/quostomize_be/common/error/ErrorCode.java
@@ -20,7 +20,12 @@ public enum ErrorCode {
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "A-201", "해당 이메일이 존재하지 않습니다."),
     EMAIL_DUPLICATED(HttpStatus.CONFLICT, "A-301", "존재하는 이메일 입니다."),
     PHONE_NUMBER_DUPLICATED(HttpStatus.CONFLICT, "A-302", "존재하는 전화번호입니다."),
-    ;
+    
+    // 카드
+    CARD_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "C-201", "해당 카드 정보를 찾을 수 없습니다."),
+    
+    // 카드 혜택
+    CARD_DETAIL_BENEFIT_NOT_FOUND(HttpStatus.NOT_FOUND, "B-201", "해당 카드에 적용된 혜택이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
