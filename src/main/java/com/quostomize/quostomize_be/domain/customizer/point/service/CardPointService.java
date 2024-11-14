8937c87faa38debd63933cfa9744ce4ebb024bed
newgamer11 <qfpotw4u81@naver.com>
2024-11-14 10:57:39 +0900

    Refactor/CardPoint: CardPointService - 수정
    ResponseEntity.ok 시 출력 될 카드 포인트 값 코드 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/point/service/CardPointService.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/point/service/CardPointService.java
@@ -19,13 +19,14 @@ public class CardPointService {
     @Autowired
     private final CardRepository cardRepository;
 
-    public void createCardPoint(Long cardId){
+    public CardPoint createCardPoint(Long cardId){
         // 카드아이디에 해당하는 카드 정보 조회
         CardDetail cardDetailById = cardRepository.findById(cardId)
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 카드 신청 내역입니다."));
         // 카드 아이디에 해당하는 카드 정보와 카드 아이디를 참조하는 카드포인트 생성
-        CardPoint cardPoint = new CardPoint(cardId,cardDetailById);
+        CardPoint cardPoint = new CardPoint(0L,cardDetailById);
         // 카드 포인트 정보 저장
         cardPointRepository.save(cardPoint);
+        return cardPoint;
     }
 }
