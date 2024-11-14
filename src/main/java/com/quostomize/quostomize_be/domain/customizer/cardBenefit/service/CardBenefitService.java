acc83bb9683d130e4c68256e483ede7031bfb598
seonmin5 <seonmin5555@gmail.com>
2024-11-14 15:43:38 +0900

    Refactor/validationErrorHandling: merge 해결

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/cardBenefit/service/CardBenefitService.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/cardBenefit/service/CardBenefitService.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
