0731cbdd24b702c0ca771f0695e27234a3ac213e
newgamer11 <qfpotw4u81@naver.com>
2024-11-14 18:41:45 +0900

    Feat/Stock: StockInterestController - 수정
    1. @Controller -> @RestController로 어노테이션 변경
    2. @RequestMapping()으로 공통 URL 기입
    3. swagger 이용을 위한 @Operation 기입
    4. 출력값이 없는 컨트롤러에 대해서도, ResponseEntity.noContent().build() 기입
    5. 기존 List출력값의 ResponseDTO<> 랩핑 기입

--- src/main/java/com/quostomize/quostomize_be/api/stock/controller/StockInterestController.java
+++ src/main/java/com/quostomize/quostomize_be/api/stock/controller/StockInterestController.java
@@ -1,15 +1,17 @@
 package com.quostomize.quostomize_be.api.stock.controller;
 
 import com.quostomize.quostomize_be.api.stock.dto.StockInterestDto;
+import com.quostomize.quostomize_be.common.dto.ResponseDTO;
 import com.quostomize.quostomize_be.domain.customizer.stock.service.StockInterestService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/stocks")
 @RequiredArgsConstructor
 public class StockInterestController {
 
@@ -17,21 +19,26 @@ public class StockInterestController {
     private final StockInterestService stockInterestService;
 
     // 조회 기능
-    @GetMapping("/api/stocks/select")
-    public ResponseEntity<List<StockInterestDto>> getStockWishList() {
+    @GetMapping("/select")
+    @Operation(summary = "위시리스토 조회",description = "현재 적용 되어있는 위시리스트를 조회합니다.")
+    public ResponseEntity<ResponseDTO<List<StockInterestDto>>> getStockWishList() {
         List<StockInterestDto> stockWishList = stockInterestService.getStockWishList();
-        return ResponseEntity.ok(stockWishList);
+        return ResponseEntity.ok(new ResponseDTO<>(stockWishList));
     }
 
     // 삭제 기능
-    @DeleteMapping("/api/stocks/select")
-    public void deleteStock(@RequestParam int order){
+    @DeleteMapping("/select")
+    @Operation(summary = "위시리스토 삭제",description = "선택한 위시항목에 대해서 삭제합니다.")
+    public ResponseEntity<ResponseDTO<List<StockInterestDto>>> deleteStock(@RequestParam int order){
         stockInterestService.deleteStock(order);
+        return ResponseEntity.noContent().build();
     }
 
     // 순위 변경 기능
-    @PatchMapping("/api/stocks/select/change-rank")
-    public void switchingStock(@RequestParam int curentOrder, @RequestParam int editOrder){
+    @PatchMapping("/select/change-rank")
+    @Operation(summary = "위시리스토 순위변경",description = "선택 되어있는 위시리스트의 순위(priority)를 변경 합니다.")
+    public ResponseEntity<ResponseDTO<List<StockInterestDto>>> switchingStock(@RequestParam int curentOrder, @RequestParam int editOrder){
         stockInterestService.switchStock(curentOrder,editOrder);
+        return  ResponseEntity.noContent().build();
     }
 }
