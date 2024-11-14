29866ae1512d1167b8c35f78a053cbed0e000917
bangsk <cats7858@gmail.com>
2024-11-14 14:48:12 +0900

    버전 업데이트

--- src/main/java/com/quostomize/quostomize_be/api/registration/dto/MemberRequestDto.java
+++ src/main/java/com/quostomize/quostomize_be/api/registration/dto/MemberRequestDto.java
@@ -55,6 +55,11 @@ public record MemberRequestDto(
                 .build();
     }
 
+
+    public boolean isValidMemberLoginId(String memberLoginId) {
+        return memberLoginId != null & memberLoginId.matches("^[a-zA-Z][a-zA-Z0-9_]*$");
+    }
+
     public boolean isValidPassword() {
         return this.memberPassword().equals(this.memberPasswordConfirm());
     }
