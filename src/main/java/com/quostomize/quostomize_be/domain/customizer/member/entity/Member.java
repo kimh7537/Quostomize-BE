7a88f477606db12a6eb6b218ecf3a46d4b999b7f
ChanUi <hcu55@naver.com>
2024-11-15 20:26:56 +0900

    Refactor/entity: Member 엔티티 수정

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/member/entity/Member.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/member/entity/Member.java
@@ -21,6 +21,9 @@ public class Member extends BaseTimeEntity {
     @Column(name = "member_id")
     private Long memberId;
 
+    @Column(name = "role", length = 20, nullable = false)
+    private String role;
+
     @Column(name = "member_name", length = 20, nullable = false)
     private String memberName;
 
