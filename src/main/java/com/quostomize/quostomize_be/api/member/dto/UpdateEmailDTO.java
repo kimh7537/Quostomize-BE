c4a788c66d509054e916b574fc06403400a0c064
Kee0304 <kee0304@gmail.com>
2024-11-15 19:16:50 +0900

    "Refactor/MyPage: Email annotation 추가"

--- src/main/java/com/quostomize/quostomize_be/api/member/dto/UpdateEmailDTO.java
+++ src/main/java/com/quostomize/quostomize_be/api/member/dto/UpdateEmailDTO.java
@@ -1,5 +1,6 @@
 package com.quostomize.quostomize_be.api.member.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -7,6 +8,7 @@ import jakarta.validation.constraints.Size;
 public record UpdateEmailDTO(
         @NotNull
         @NotBlank
+        @Email
         @Size(max=50)
         String newEmail
 ) {
