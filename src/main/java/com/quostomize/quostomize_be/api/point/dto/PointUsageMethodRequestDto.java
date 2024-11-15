fd0f5fd166897b754abda5bd90d22cde7bdcf420
ChanUi <hcu55@naver.com>
2024-11-15 20:20:49 +0900

    Refactor/validation: PointUsageMethodRequestDto 검증 추가

--- src/main/java/com/quostomize/quostomize_be/api/point/dto/PointUsageMethodRequestDto.java
+++ src/main/java/com/quostomize/quostomize_be/api/point/dto/PointUsageMethodRequestDto.java
@@ -3,13 +3,15 @@ package com.quostomize.quostomize_be.api.point.dto;
 import com.quostomize.quostomize_be.api.lotto.dto.LottoParticipantRequestDto;
 import com.quostomize.quostomize_be.domain.customizer.card.entity.CardDetail;
 import com.quostomize.quostomize_be.domain.customizer.point.entity.PointUsageMethod;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record PointUsageMethodRequestDto(
-        Long pointUsageTypeId,
-        CardDetail cardDetail,
-        Boolean isLotto,
-        Boolean isPayback,
-        Boolean isPieceStock
+        @Positive Long pointUsageTypeId,
+        @NotNull CardDetail cardDetail,
+        @NotNull Boolean isLotto,
+        @NotNull Boolean isPayback,
+        @NotNull Boolean isPieceStock
 ) {
     public static PointUsageMethodRequestDto from(PointUsageMethod pointUsageMethod, LottoParticipantRequestDto request) {
         return new PointUsageMethodRequestDto(
