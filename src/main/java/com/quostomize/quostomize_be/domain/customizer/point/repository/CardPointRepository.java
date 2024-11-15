c0548dcfbc714269735e047238b28e579ab39dd1
ChanUi <hcu55@naver.com>
2024-11-15 20:19:32 +0900

    Refactor/repository: CardPointRepository Optional 처리

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/point/repository/CardPointRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/point/repository/CardPointRepository.java
@@ -3,6 +3,8 @@ package com.quostomize.quostomize_be.domain.customizer.point.repository;
 import com.quostomize.quostomize_be.domain.customizer.point.entity.CardPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CardPointRepository extends JpaRepository<CardPoint, Long> {
-    CardPoint findByCardDetail_CardSequenceId(Long cardSequenceId);
+    Optional<CardPoint> findByCardDetail_CardSequenceId(Long cardSequenceId);
 }
