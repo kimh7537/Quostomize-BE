2ebaa5e0955526c88039426f74584e6e780344e3
kimh7537 <kimh7537@gmail.com>
2024-11-15 20:12:42 +0900

    Feat/auth: auth member 관련 저장소 서비스 엔티티 추가

---
+++ src/main/java/com/quostomize/quostomize_be/domain/auth/entity/Member.java
@@ -0,0 +1,93 @@
+package com.quostomize.quostomize_be.domain.auth.entity;
+
+import com.quostomize.quostomize_be.common.entity.BaseTimeEntity;
+import com.quostomize.quostomize_be.domain.auth.enums.MemberRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "members", uniqueConstraints = {
+        @UniqueConstraint(name = "MEMBER_EMAIL_UNIQUE", columnNames = "member_email"),
+        @UniqueConstraint(name = "MEMBER_LOGIN_ID_UNIQUE", columnNames = "member_login_id"),
+        @UniqueConstraint(name = "RESIDENCE_NUMBER_UNIQUE", columnNames = "residence_number"),
+        @UniqueConstraint(name = "MEMBER_PHONE_NUMBER_UNIQUE", columnNames = "member_phone_number")
+})
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "member_name", length = 20, nullable = false)
+    private String memberName;
+
+    @Email
+    @Column(name = "member_email", length = 50, nullable = false)
+    private String memberEmail;
+
+    @Column(name = "member_login_id", length = 15, nullable = false)
+    private String memberLoginId;
+
+    @Column(name = "member_password", nullable = false)
+    private String memberPassword;
+
+    @Column(name = "residence_number", nullable = false)
+    private String residenceNumber;
+
+    @Column(name = "zip_code", length = 10, nullable = false)
+    private String zipCode;
+
+    @Column(name = "member_address", length = 100, nullable = false)
+    private String memberAddress;
+
+    @Column(name = "member_detail_address", length = 100, nullable = false)
+    private String memberDetailAddress;
+
+    @Column(name = "member_phone_number", nullable = false)
+    private String memberPhoneNumber;
+
+    @Column(name = "secondary_auth_code", nullable = false)
+    private String secondaryAuthCode;
+
+    @Column(name = "member_role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault(value = "'MEMBER'")
+    private MemberRole role;
+
+    @Builder
+    public Member(String memberLoginId, String memberPassword, String memberName, String memberPhoneNumber, String memberEmail){
+        this.memberLoginId = memberLoginId;
+        this.memberPassword = memberPassword;
+        this.memberName = memberName;
+        this.memberPhoneNumber = memberPhoneNumber;
+        this.memberEmail = memberEmail;
+    }
+
+    public void updateRole(MemberRole role) {
+        this.role = role;
+    }
+
+    public void updatePassword(String memberPassword) {
+        this.memberPassword = memberPassword;
+    }
+
+    public void updateSecondaryAuthCode(String secondaryAuthCode) {
+        this.secondaryAuthCode = secondaryAuthCode;
+    }
+
+    public void updatePhoneNumber(String memberPhoneNumber) {
+        this.memberPhoneNumber = memberPhoneNumber;
+    }
+
+
+}
