c9c50ce3642b5994c80dbba58c249c5b64747310
ChanUi <hcu55@naver.com>
2024-11-15 11:18:51 +0900

    Refactor/smsRequest: SMS 인증 dto record 변경

--- src/main/java/com/quostomize/quostomize_be/api/sms/dto/SmsRequest.java
+++ src/main/java/com/quostomize/quostomize_be/api/sms/dto/SmsRequest.java
@@ -2,19 +2,14 @@ package com.quostomize.quostomize_be.api.sms.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
 
-public class UserDto {
-
-    @Getter
-    public static class SmsCertificationRequest {
-
+public record SmsRequest(
         @NotBlank(message = "전화번호는 필수 입력 값입니다.")
         @Pattern(regexp = "^010\\d{8}$", message = "전화번호 형식이 올바르지 않습니다.")
-        private String phone;
+        String phone,
 
         @NotBlank(message = "인증번호는 필수 입력 값입니다.")
         @Pattern(regexp = "^\\d{6}$", message = "인증번호는 6자리 숫자여야 합니다.")
-        private String certificationNumber;
-    }
+        String certificationNumber
+) {
 }
