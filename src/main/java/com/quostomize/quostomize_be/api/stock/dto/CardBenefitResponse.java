6b50714cc598cd629dc5f5dfab3cd2236ab8b383
newgamer11 <qfpotw4u81@naver.com>
2024-11-17 23:32:20 +0900

    Refactor/Stock: CardBenefitResponse - 수정
    @NotNull @NotBlank 어노테이션 추가

--- src/main/java/com/quostomize/quostomize_be/api/stock/dto/CardBenefitResponse.java
+++ src/main/java/com/quostomize/quostomize_be/api/stock/dto/CardBenefitResponse.java
@@ -1,9 +1,18 @@
 package com.quostomize.quostomize_be.api.stock.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CardBenefitResponse(
+     @NotNull
      Boolean isActive, // 활성화여부
+     @NotNull
      Integer benefitRate, // 혜택률
+     @NotNull
+     @NotBlank
      String benefitCategoryType, // 상위분류
+     @NotNull
+     @NotBlank
      String franchiseName // 하위분류
 
 ) {
