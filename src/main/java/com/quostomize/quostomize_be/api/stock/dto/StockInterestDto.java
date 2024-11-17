0bde4785ec1c73702a13b126d3d78d0b5c7a48c5
newgamer11 <qfpotw4u81@naver.com>
2024-11-17 23:34:16 +0900

    Refactor/Stock: StockInterestDto - 어노테이션 추가
    @NotNull @NotBlank 어노테이션 추가

--- src/main/java/com/quostomize/quostomize_be/api/stock/dto/StockInterestDto.java
+++ src/main/java/com/quostomize/quostomize_be/api/stock/dto/StockInterestDto.java
@@ -1,9 +1,18 @@
 package com.quostomize.quostomize_be.api.stock.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record StockInterestDto (
+        @NotNull
         Integer priority,
+        @NotNull
+        @NotBlank
         String stockName,
+        @NotNull
         Integer stockPresentPrice,
+        @NotNull
+        @NotBlank
         String stockImage
 ) {
     public StockInterestDto withStockImage(String newStockImage) {
