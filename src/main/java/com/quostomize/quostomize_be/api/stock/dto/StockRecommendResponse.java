e5ff54e0fb5dea4842aaa874b56a3a71b3d2ea40
newgamer11 <qfpotw4u81@naver.com>
2024-11-17 23:33:40 +0900

    Refactor/Stock: StockRecommendResponse - 수정
    @NotNull @NotBlank 어노테이션 추가

--- src/main/java/com/quostomize/quostomize_be/api/stock/dto/StockRecommendResponse.java
+++ src/main/java/com/quostomize/quostomize_be/api/stock/dto/StockRecommendResponse.java
@@ -1,8 +1,16 @@
 package com.quostomize.quostomize_be.api.stock.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record StockRecommendResponse(
+        @NotNull
+        @NotBlank
         String stockName,
+        @NotNull
         Integer stockPresentPrice,
+        @NotNull
+        @NotBlank
         String stockImage
 ) {
     public StockRecommendResponse(String stockName, Integer stockPresentPrice, String stockImage) {
