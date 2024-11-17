9f83e91269f5d91bdbbc77000200ecf6a2b76ef6
seonmin5 <seonmin5555@gmail.com>
2024-11-17 22:11:03 +0900

    Feat/inquiryRegistration: MemberQuestionController 로그인 로직 추가

--- src/main/java/com/quostomize/quostomize_be/api/memberQuestion/controller/MemberQuestionController.java
+++ src/main/java/com/quostomize/quostomize_be/api/memberQuestion/controller/MemberQuestionController.java
@@ -2,29 +2,30 @@ package com.quostomize.quostomize_be.api.memberQuestion.controller;
 
 import com.quostomize.quostomize_be.api.memberQuestion.dto.MemberQuestionRequest;
 import com.quostomize.quostomize_be.common.dto.ResponseDTO;
-import com.quostomize.quostomize_be.domain.customizer.member.entity.Member;
-import com.quostomize.quostomize_be.memberQuestion.service.MemberQuestionService;
+import com.quostomize.quostomize_be.domain.auth.entity.Member;
+import com.quostomize.quostomize_be.domain.customizer.memberQuestion.service.MemberQuestionService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/qnas")
 public class MemberQuestionController {
-
     private final MemberQuestionService memberQuestionService;
 
     public MemberQuestionController(MemberQuestionService memberQuestionService) {
         this.memberQuestionService = memberQuestionService;
     }
 
-    @PostMapping("/{id}")
+    @PostMapping("")
+    @Operation(summary = "문의 등록", description = "로그인한 회원에 한해서만 문의사항을 등록할 수 있습니다.")
     public ResponseEntity<ResponseDTO> createQuestion(
-            @PathVariable Long id,
+            @AuthenticationPrincipal Long memberId,
             @RequestBody MemberQuestionRequest request) {
 
-        Member member = memberQuestionService.getMemberById(id);
+        Member member = memberQuestionService.getMemberById(memberId);
         Long questionId = memberQuestionService.createQuestion(request, member);
-//        return ResponseEntity.ok(new ResponseDTO<>(response));
         return ResponseEntity.ok(new ResponseDTO<>(questionId));
     }
 }
