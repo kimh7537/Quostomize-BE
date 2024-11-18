eff64b7b0124792302ad093fe57be06d40008793
seonmin5 <seonmin5555@gmail.com>
2024-11-18 14:34:08 +0900

    Refactor/adminInquiryResponse: AdminResponseController ADMIN만 문의 답변 등록

--- src/main/java/com/quostomize/quostomize_be/api/adminResponse/controller/AdminResponseController.java
+++ src/main/java/com/quostomize/quostomize_be/api/adminResponse/controller/AdminResponseController.java
@@ -3,7 +3,10 @@ package com.quostomize.quostomize_be.api.adminResponse.controller;
 import com.quostomize.quostomize_be.api.adminResponse.dto.AdminResponseRequest;
 import com.quostomize.quostomize_be.common.dto.ResponseDTO;
 import com.quostomize.quostomize_be.domain.customizer.adminResponse.service.AdminResponseService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,16 +20,21 @@ public class AdminResponseController {
     }
 
     // 문의에 대한 답변 등록
-    @PostMapping("/{questionsSequenceId}/admin-answer")
-    // TODO: RequestBody 및 Admin 구분 로직에 Authentication 추가 필요
+    @PostMapping("/{questionsSequenceId}/answer")
+    @Operation(summary = "문의 답변 등록", description = "ADMIN 권한만 답변 등록 가능합니다.")
     public ResponseEntity<ResponseDTO> createAnswer(
+            Authentication authentication,
             @PathVariable("questionsSequenceId") Long questionsSequenceId,
             @RequestBody AdminResponseRequest adminRequest) {
-        boolean isAdmin = true;
-        if (!isAdmin) {
-            return ResponseEntity.badRequest().body(new ResponseDTO<>("Admin 권한이 필요합니다."));
-        }
-        adminResponseService.createAdminAnswer(questionsSequenceId, adminRequest.responseContent());
-        return ResponseEntity.ok(new ResponseDTO<>(questionsSequenceId));
+
+        Long memberId = (Long) authentication.getPrincipal();
+        String memberRole = authentication.getAuthorities()
+                .stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("UNKNOWN");
+        
+        Long responseId = adminResponseService.createAdminAnswer(memberId, memberRole, questionsSequenceId, adminRequest.responseContent());
+        return ResponseEntity.ok(new ResponseDTO<>(responseId));
     }
 }
