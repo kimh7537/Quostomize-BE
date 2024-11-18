f33edd0c75077699bddb41deb30eb63005131b04
seonmin5 <seonmin5555@gmail.com>
2024-11-18 10:48:05 +0900

    Feat/inquiryListSearch: MemberQuestionRepository members.memberId 조회 쿼리 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/memberQuestion/repository/MemberQuestionRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/memberQuestion/repository/MemberQuestionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberQuestionRepository extends JpaRepository<MemberQuestion, Integer> {
+public interface MemberQuestionRepository extends JpaRepository<MemberQuestion, Long> {
     Page<MemberQuestion> findAll(Pageable pageable);
+    Page<MemberQuestion> findByMember_MemberId(Long memberId, Pageable pageable);
 }
