ae7ff43582dd5d3515a75c8cfb24c42c5e1e011b
seonmin5 <seonmin5555@gmail.com>
2024-11-18 19:16:01 +0900

    Refactor/adminInquiryResponse: MemberQuestionRepository EntityGraph 추가하여 Lazy Loading 문제 해결

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/memberQuestion/repository/MemberQuestionRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/memberQuestion/repository/MemberQuestionRepository.java
@@ -3,13 +3,16 @@ package com.quostomize.quostomize_be.domain.customizer.memberQuestion.repository
 import com.quostomize.quostomize_be.domain.customizer.memberQuestion.entity.MemberQuestion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberQuestionRepository extends JpaRepository<MemberQuestion, Long> {
     Page<MemberQuestion> findAll(Pageable pageable);
     Page<MemberQuestion> findByMember_MemberId(Long memberId, Pageable pageable);
+    @EntityGraph(attributePaths = "member")
     Optional<MemberQuestion> findByMember_MemberIdAndQuestionsSequenceId(Long memberId, Long questionsSequenceId);
-    Optional<MemberQuestion> findByMember_MemberId(Long questionSequenceId);
 }
