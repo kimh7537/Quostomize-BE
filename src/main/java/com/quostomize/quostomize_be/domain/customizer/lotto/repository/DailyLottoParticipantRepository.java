37ea8f11f6ef00677f3123b218680804c9f8dcb0
ChanUi <hcu55@naver.com>
2024-11-18 18:38:16 +0900

    Feat/dailyLottoRepository: DailyLottoParticipantRepository auto_increment 초기화 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/lotto/repository/DailyLottoParticipantRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/lotto/repository/DailyLottoParticipantRepository.java
@@ -3,9 +3,15 @@ package com.quostomize.quostomize_be.domain.customizer.lotto.repository;
 import com.quostomize.quostomize_be.domain.customizer.customer.entity.Customer;
 import com.quostomize.quostomize_be.domain.customizer.lotto.entity.DailyLottoParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface DailyLottoParticipantRepository extends JpaRepository<DailyLottoParticipant, Long> {
     Optional<DailyLottoParticipant> findByCustomer(Customer customer);
+
+    @Modifying
+    @Query(value = "ALTER TABLE daily_lotto_participant AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
 }
