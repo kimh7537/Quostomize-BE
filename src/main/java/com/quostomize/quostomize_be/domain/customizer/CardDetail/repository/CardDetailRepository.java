2a577823797f4fabadceff5a03d16ea3f1ad098e
ChanUi <hcu55@naver.com>
2024-11-18 18:22:47 +0900

    Remove/cardDetailRepository: CardDetailRepository 삭제 (CardRepository 중복)

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/CardDetail/repository/CardDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quostomize.quostomize_be.domain.customizer.CardDetail.repository;
-
-import com.quostomize.quostomize_be.domain.customizer.card.entity.CardDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//
-public interface CardDetailRepository extends JpaRepository<CardDetail, Long> {}
