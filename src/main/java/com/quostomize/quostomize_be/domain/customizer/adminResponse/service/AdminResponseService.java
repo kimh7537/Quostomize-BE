de9c02173e1e8eb11844b7d622144b0bdf1b4b34
seonmin5 <seonmin5555@gmail.com>
2024-11-18 14:33:03 +0900

    Refactor/adminInquiryResponse: AdminResponseService ADMIN 권한 여부 확인 후 답변 등록

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/adminResponse/service/AdminResponseService.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/adminResponse/service/AdminResponseService.java
@@ -1,12 +1,16 @@
 package com.quostomize.quostomize_be.domain.customizer.adminResponse.service;
 
+import com.quostomize.quostomize_be.common.error.ErrorCode;
+import com.quostomize.quostomize_be.common.error.exception.AppException;
 import com.quostomize.quostomize_be.domain.customizer.adminResponse.entity.AdminResponse;
 import com.quostomize.quostomize_be.domain.customizer.adminResponse.repository.AdminResponseRepository;
 import com.quostomize.quostomize_be.domain.customizer.memberQuestion.entity.MemberQuestion;
 import com.quostomize.quostomize_be.domain.customizer.memberQuestion.repository.MemberQuestionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class AdminResponseService {
 
     private final AdminResponseRepository adminResponseRepository;
@@ -18,20 +22,34 @@ public class AdminResponseService {
     }
 
     // 문의에 대한 답변 등록
-    public Long createAdminAnswer(Long questionsSequenceId, String responseContent) {
-        MemberQuestion memberQuestion = memberQuestionRepository.findById(questionsSequenceId).orElseThrow(() -> new IllegalArgumentException("해당하는 문의글 없음"));
+    // TODO: Repository 직접 참조하지 않고, 연관된 Service를 통해서 참조하는 방식으로 변경
+    @Transactional
+    public Long createAdminAnswer(Long memberId, String memberRole, Long questionsSequenceId, String responseContent) {
+        // ADMIN 권한 여부 확인
+        if (!"ROLE_ADMIN".equals(memberRole)) {
+            throw new AppException(ErrorCode.USER_ACCESS_DENIED);
+        }
+
+        // 문의글 확인
+        MemberQuestion memberQuestion = memberQuestionRepository.findById(questionsSequenceId).orElseThrow(() -> new AppException(ErrorCode.ENTITY_NOT_FOUND));
         
-        // 해당 문의글에 대한 답변 존재 여부 확인
+        // 답변 중복 여부 확인
         adminResponseRepository.findByMemberQuestion(memberQuestion)
                 .ifPresent(adminResponse -> {
-                    throw new IllegalStateException("이미 답변이 등록된 문의글임");
+                    throw new AppException(ErrorCode.DUPLICATE_REQUEST);
                 });
         
+        // 답변 저장
         AdminResponse adminResponse = AdminResponse.builder()
                 .responseContent(responseContent)
                 .memberQuestion(memberQuestion)
                 .build();
         adminResponseRepository.save(adminResponse);
+
+        // isAnswered = true로 업데이트
+        memberQuestion.markAsAnswered();
+        memberQuestionRepository.save(memberQuestion);
+
         return adminResponse.getResponseSequenceId();
     }
 }
