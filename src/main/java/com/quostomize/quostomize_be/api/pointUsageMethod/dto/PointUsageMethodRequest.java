a24701667d247736e8a0f4e51b47b22954785f42
ChanUi <hcu55@naver.com>
2024-11-18 18:28:27 +0900

    Refactor/dto: PointUsageMethodRequestDto 이름 변경 (중복으로 인해)

--- src/main/java/com/quostomize/quostomize_be/api/pointUsageMethod/dto/PointUsageMethodRequest.java
+++ src/main/java/com/quostomize/quostomize_be/api/pointUsageMethod/dto/PointUsageMethodRequest.java
@@ -1,20 +1,20 @@
-package com.quostomize.quostomize_be.api.point.dto;
+package com.quostomize.quostomize_be.api.pointUsageMethod.dto;
 
 import com.quostomize.quostomize_be.api.lotto.dto.LottoParticipantRequestDto;
 import com.quostomize.quostomize_be.domain.customizer.card.entity.CardDetail;
-import com.quostomize.quostomize_be.domain.customizer.point.entity.PointUsageMethod;
+import com.quostomize.quostomize_be.domain.customizer.pointUsageMethod.entity.PointUsageMethod;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record PointUsageMethodRequestDto(
+public record PointUsageMethodRequest(
         @Positive Long pointUsageTypeId,
         @NotNull CardDetail cardDetail,
         @NotNull Boolean isLotto,
         @NotNull Boolean isPayback,
         @NotNull Boolean isPieceStock
 ) {
-    public static PointUsageMethodRequestDto from(PointUsageMethod pointUsageMethod, LottoParticipantRequestDto request) {
-        return new PointUsageMethodRequestDto(
+    public static PointUsageMethodRequest from(PointUsageMethod pointUsageMethod, LottoParticipantRequestDto request) {
+        return new PointUsageMethodRequest(
                 pointUsageMethod.getPointUsageTypeId(),
                 pointUsageMethod.getCardDetail(),
                 request.isLottoOn(),
