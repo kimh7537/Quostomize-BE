b69857f4b7782b99dee8c0856f2678cc16cdad2a
ChanUi <hcu55@naver.com>
2024-11-18 19:24:56 +0900

    Refactor/pointUsageMethodRepository: 중복된 파일 병합

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/pointUsageMethod/repository/PointUsageMethodRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/pointUsageMethod/repository/PointUsageMethodRepository.java
@@ -4,10 +4,12 @@ import com.quostomize.quostomize_be.domain.customizer.pointUsageMethod.entity.Po
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PointUsageMethodRepository extends JpaRepository <PointUsageMethod, Long> {
     Optional<PointUsageMethod> findByCardDetail_CardSequenceId(Long cardSequenceId);
 
+    List<PointUsageMethod> findAllByIsLottoTrue();
 }
