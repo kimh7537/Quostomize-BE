53e23596c3512f2d6aab024e7394c203e2ff1d73
newgamer11 <qfpotw4u81@naver.com>
2024-11-18 18:25:42 +0900

    Feat/Stock: AcquiredCardBenefits -생성 (CardBenefitResponse Dto 생성코드)

---
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/stock/common/AcquiredCardBenefits.java
@@ -0,0 +1,29 @@
+package com.quostomize.quostomize_be.domain.customizer.stock.common;
+
+import com.quostomize.quostomize_be.api.stock.dto.CardBenefitResponse;
+import com.quostomize.quostomize_be.domain.customizer.benefit.entity.CardBenefit;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AcquiredCardBenefits {
+
+    public List<CardBenefitResponse> getCardBenefitDto(List<CardBenefit> cardBenefits){
+
+        List<CardBenefitResponse> benefits = cardBenefits.stream()
+                .map(Benefit -> {
+                    String benefitCategoryType = Benefit.getUpperCategory().getBenefitCategoryType();
+                    String franchiseName = Benefit.getLowerCategory() != null
+                            ? Benefit.getLowerCategory().getBenefitCategoryType()
+                            : null; // 하위타입이 없으면 null을 넣는다
+                    return new CardBenefitResponse(
+                            Benefit.getIsActive(),
+                            Benefit.getBenefitRate(),
+                            benefitCategoryType,
+                            franchiseName
+                    );
+                }).collect(Collectors.toList());
+
+        return benefits;
+    }
+}
