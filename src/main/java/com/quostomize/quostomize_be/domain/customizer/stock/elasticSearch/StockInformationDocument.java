91d0ca2048fe78c4214dba205fe580360a7de5e9
kimh7537 <kimh7537@gmail.com>
2024-11-19 09:36:27 +0900

    Feat/elk: elastiSearch Document 값 수정

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/stock/elasticSearch/StockInformationDocument.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/stock/elasticSearch/StockInformationDocument.java
@@ -4,29 +4,37 @@ import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.data.elasticsearch.annotations.*;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @ToString
 @Builder
-@Document(indexName = "stockInformation")
 @Mapping(mappingPath = "static/elastic-mapping.json")
-//@Setting(settingPath = "static/elastic-token.json")
+@Setting(settingPath = "static/elastic-token.json")
+@Document(indexName = "stock_informations")
 public class StockInformationDocument {
-
     @Id
-    @Field(type = FieldType.Keyword)  // Elasticsearch에서 ID는 일반적으로 Keyword 타입
-    private String id;
+    @Field(type = FieldType.Keyword)
+    private Long id;
 
     @Field(type = FieldType.Integer)
     private Integer stockCode;
 
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, analyzer = "nori_analyzer", searchAnalyzer = "nori_analyzer")
     private String stockName;
 
     @Field(type = FieldType.Integer)
     private Integer stockPresentPrice;
 
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text, index = false)
     private String stockImage;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_time)
+    private String createdAt;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_time)
+    private String modifiedAt;
+
 }
--- src/main/java/com/quostomize/quostomize_be/domain/customizer/stock/elasticSearch/StockInformationSearchService.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/stock/elasticSearch/StockInformationSearchService.java
@@ -1,15 +1,19 @@
 package com.quostomize.quostomize_be.domain.customizer.stock.elasticSearch;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StockInformationSearchService {
 
     private final StockInformationDocumentRepository stockInformationRepository;
+    private final ElasticsearchTemplate elasticsearchTemplate;
 
     public StockInformationDocument createStockInformationDocument(StockInformationDocument stockInformationDocument) {
         return stockInformationRepository.save(stockInformationDocument);
@@ -19,4 +23,5 @@ public class StockInformationSearchService {
         List<StockInformationDocument> stockInformationDocuments = stockInformationRepository.findByStockName(keyword);
         return stockInformationDocuments;
     }
+
 }
