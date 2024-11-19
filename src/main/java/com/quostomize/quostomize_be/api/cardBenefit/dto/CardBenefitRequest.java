23c809be8f04ca55c455668606f59eb06b03f24f
ChanUi <hcu55@naver.com>
2024-11-19 16:53:35 +0900

    Feat/dto: CardBenefitRequest 2차 비밀번호 dto 추가

--- src/main/java/com/quostomize/quostomize_be/api/cardBenefit/dto/CardBenefitRequest.java
+++ src/main/java/com/quostomize/quostomize_be/api/cardBenefit/dto/CardBenefitRequest.java
@@ -1,7 +1,8 @@
 package com.quostomize.quostomize_be.api.cardBenefit.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Range;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,13 +10,13 @@ import java.util.List;
 public record CardBenefitRequest(
         LocalDate benefitEffectiveDate,
         Integer benefitRate,
-        @NotNull
-        Boolean isActive,
-        @NotNull
-        Long cardSequenceId,
-        @NotNull
-        Long upperCategoryId,
-        Long lowerCategoryId
+        @NotNull Boolean isActive,
+        @NotNull Long cardSequenceId,
+        @NotNull Long upperCategoryId,
+        Long lowerCategoryId,
+        @NotBlank(message = "2차 비밀번호는 필수 입력값입니다.")
+        @Pattern(regexp = "^[0-9]{6}$", message = "2차 비밀번호는 6자리 숫자여야 합니다.")
+        String secondaryAuthCode
 ) {
     // benefitEffectiveDate가 null이면 now를 반환하도록
     public LocalDate benefitEffectiveDate() {
