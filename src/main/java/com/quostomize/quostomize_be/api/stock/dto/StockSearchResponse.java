3f4886424354c3752c750dea134c64202a30144c
kimh7537 <kimh7537@gmail.com>
2024-11-22 11:07:07 +0900

    Refactor/stock: stockInformation 정보를 검색하는 api 구현(JPA)

--- src/main/java/com/quostomize/quostomize_be/api/stock/dto/StockSearchResponse.java
+++ src/main/java/com/quostomize/quostomize_be/api/stock/dto/StockSearchResponse.java
@@ -1,5 +1,9 @@
 package com.quostomize.quostomize_be.api.stock.dto;
 
+import com.quostomize.quostomize_be.api.cardBenefit.dto.CardBenefitResponse;
+import com.quostomize.quostomize_be.domain.customizer.cardBenefit.entity.CardBenefit;
+import com.quostomize.quostomize_be.domain.customizer.stock.entity.StockInformation;
+
 public record StockSearchResponse(
         Long stockInformationId,
         Integer stockCode,
@@ -7,4 +11,15 @@ public record StockSearchResponse(
         Integer stockPresentPrice,
         String stockImage
 ) {
+
+    public static StockSearchResponse from(StockInformation stockInformation){
+        return new StockSearchResponse(
+                stockInformation.getStockInformationId(),
+                stockInformation.getStockCode(),
+                stockInformation.getStockName(),
+                stockInformation.getStockPresentPrice(),
+                stockInformation.getStockImage()
+        );
+    }
+
 }
