aec50fb33c67b59517f364215875ef5f4d588a83
seonmin5 <seonmin5555@gmail.com>
2024-11-28 00:05:28 +0900

    Feat/cardInfo: CardDetailRepository findByStatus 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
@@ -1,6 +1,7 @@
 package com.quostomize.quostomize_be.domain.customizer.card.repository;
 
 import com.quostomize.quostomize_be.domain.customizer.card.entity.CardDetail;
+import com.quostomize.quostomize_be.domain.customizer.card.enums.CardStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CardDetailRepository extends JpaRepository<CardDetail, Long> {
     Page<CardDetail> findAll(Pageable pageable);
+    Page<CardDetail> findByStatus(Pageable pageable, CardStatus status);
 }
