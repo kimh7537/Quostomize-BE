acfe46dbf8d4d66ac671833687803d8c03f9b554
seonmin5 <seonmin5555@gmail.com>
2024-11-28 06:32:04 +0900

    Fix/dev: CardStatus 오타 수정(CREATION_PENDING), key 및 description 추가, string으로 변환

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/card/enums/CardStatus.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/card/enums/CardStatus.java
@@ -3,11 +3,31 @@ package com.quostomize.quostomize_be.domain.customizer.card.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Getter
 @RequiredArgsConstructor
 public enum CardStatus {
-    CREARTION_PENDING,
-    ACTIVE,
-    CANCELLATION_PENDING,
-    CANCELLED;
+    CREATION_PENDING("CREATION_PENDING", "카드 신청 대기"),
+    ACTIVE("ACTIVE", "카드 이용 가능"),
+    CANCELLATION_PENDING("CANCELLATION_PENDING", "카드 해지 대기"),
+    CANCELLED("CANCELLED", "카드 해지");
+
+    private static final Map<String, CardStatus> STATUS_KEY_MAP = Stream.of(values())
+            .collect(Collectors.toUnmodifiableMap(CardStatus::getKey, Function.identity()));
+
+    private final String key;
+    private final String description;
+
+    public static CardStatus fromKey(final String key) {
+        CardStatus status = STATUS_KEY_MAP.get(key);
+        if (status == null) {
+            throw new IllegalArgumentException(String.format("요청한 key(%s)를 찾을 수 없습니다.", key));
+        }
+        return status;
+    }
 }
