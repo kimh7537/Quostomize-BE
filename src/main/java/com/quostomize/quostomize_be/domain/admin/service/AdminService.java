eab6d52d390dd1859adda65e486b7d3fa987e3e4
seonmin5 <seonmin5555@gmail.com>
2024-11-27 20:34:35 +0900

    Feat/cardInfo: AdminService getCardDetailsForAdmin  서비스 추가

---
+++ src/main/java/com/quostomize/quostomize_be/domain/admin/service/AdminService.java
@@ -0,0 +1,37 @@
+package com.quostomize.quostomize_be.domain.admin.service;
+
+import com.quostomize.quostomize_be.api.card.dto.CardDetailResponse;
+import com.quostomize.quostomize_be.common.error.ErrorCode;
+import com.quostomize.quostomize_be.common.error.exception.AppException;
+import com.quostomize.quostomize_be.domain.customizer.card.service.CardService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly=true)
+public class AdminService {
+
+    private final CardService cardService;
+
+    public AdminService(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    public Page<CardDetailResponse> getCardDetailsForAdmin(Authentication authentication, int page) {
+        String memberRole = authentication.getAuthorities()
+                .stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("UNKNOWN");
+        if (!"ROLE_ADMIN".equals(memberRole)) {
+            throw new AppException(ErrorCode.ENTITY_NOT_FOUND);
+        }
+        Pageable pageable = PageRequest.of(page, 20);
+        return cardService.findPagedCardDetails(pageable);
+    }
+}
