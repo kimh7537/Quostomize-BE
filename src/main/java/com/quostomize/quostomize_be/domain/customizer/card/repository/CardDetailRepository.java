2148a28e241678b2dbc4ccac6f216acf1eb4b086
seonmin5 <seonmin5555@gmail.com>
2024-11-28 06:59:02 +0900

    Feat/cardInfo: CardDetailRepository findyBySearchTerm 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
@@ -5,10 +5,14 @@ import com.quostomize.quostomize_be.domain.customizer.card.enums.CardStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardDetailRepository extends JpaRepository<CardDetail, Long> {
     Page<CardDetail> findAll(Pageable pageable);
     Page<CardDetail> findByStatus(Pageable pageable, CardStatus status);
+    @Query("select c from CardDetail c where c.cardNumber like concat('%', :searchTerm) ")
+    Page<CardDetail> findBySearchTerm(Pageable pageable, @Param("searchTerm") String searchTerm);
 }
