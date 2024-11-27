4a9a57cf9a99b5153eeb4b9acaf2378f195bdce0
seonmin5 <seonmin5555@gmail.com>
2024-11-27 20:29:07 +0900

    Feat/cardInfo: CardDetailRepository Page findall 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
@@ -1,7 +1,12 @@
 package com.quostomize.quostomize_be.domain.customizer.card.repository;
 
 import com.quostomize.quostomize_be.domain.customizer.card.entity.CardDetail;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CardDetailRepository extends JpaRepository<CardDetail, Long> {
+    Page<CardDetail> findAll(Pageable pageable);
 }
