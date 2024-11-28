cc95c4f0d8414f11ca239d8d5a80a5a427aadd77
seonmin5 <seonmin5555@gmail.com>
2024-11-28 20:20:42 +0900

    Feat/paymentRecordInfo: PaymentRecordService Greater, Less, Equal에 대한 서비스 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/payment/service/PaymentRecordService.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/payment/service/PaymentRecordService.java
@@ -16,4 +16,8 @@ public class PaymentRecordService {
     private final PaymentRecordRepository paymentRecordRepository;
 
     public Page<PaymentRecord> getPagedRecords(Pageable pageable) {return paymentRecordRepository.findAll(pageable);}
+    public Page<PaymentRecord> getGreaterRecords(Pageable pageable, Long amount) {return paymentRecordRepository.findByPaymentAmountGreater(pageable, amount);}
+    public Page<PaymentRecord> getLessRecords(Pageable pageable, Long amount) {return paymentRecordRepository.findByPaymentAmountLess(pageable, amount);}
+    public Page<PaymentRecord> getEqualRecords(Pageable pageable, Long amount) {return paymentRecordRepository.findByPaymentAmountEqual(pageable, amount);}
+
 }
