ae13370af121f291bf703056d0c7087c9d81f2d3
seonmin5 <seonmin5555@gmail.com>
2024-11-28 15:28:01 +0900

    Feat/cardStatusEdit: CArdDetailRepository에 updateStatus 쿼리문 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/card/repository/CardDetailRepository.java
@@ -5,6 +5,7 @@ import com.quostomize.quostomize_be.domain.customizer.card.enums.CardStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,8 @@ public interface CardDetailRepository extends JpaRepository<CardDetail, Long> {
     Page<CardDetail> findByStatus(Pageable pageable, CardStatus status);
     @Query("select c from CardDetail c where c.cardNumber like concat('%', :searchTerm) ")
     Page<CardDetail> findBySearchTerm(Pageable pageable, @Param("searchTerm") String searchTerm);
+
+    @Modifying
+    @Query("update CardDetail cd set cd.status = :newStatus where cd.cardSequenceId = :cardSequenceId")
+    void updateStatus(@Param("newStatus") CardStatus newStatus, @Param("cardSequenceId") Long cardSequenceId);
 }
