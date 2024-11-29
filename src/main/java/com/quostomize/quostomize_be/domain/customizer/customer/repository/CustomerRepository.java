f7a781b07ca874baf5e5460cf908181b27dd7e84
kimh7537 <kimh7537@gmail.com>
2024-11-29 10:50:57 +0900

    Feat/auth: CardStatus를 찾아주는 쿼리 생성

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/customer/repository/CustomerRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/customer/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.quostomize.quostomize_be.domain.customizer.customer.repository;
 
+import com.quostomize.quostomize_be.domain.auth.entity.Member;
 import com.quostomize.quostomize_be.domain.customizer.customer.entity.Customer;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,5 +22,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
             "WHERE m.memberId = :memberId")
     Optional<Customer> findCustomerWithLock(@Param("memberId") Long memberId);
 
+    @Query("SELECT c FROM Customer c JOIN FETCH c.cardDetail WHERE c.member = :member")
+    Optional<Customer> findWithCardDetailByMember(@Param("member") Member member);
 
 }
