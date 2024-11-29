e58626a48322bb5ee1fa597a14db3f79ddb31c21
seonmin5 <seonmin5555@gmail.com>
2024-11-29 10:22:19 +0900

    Feat/cardCreationInfo: CardApplicantInfoRepository findByCardStatus 쿼리문 추가

--- src/main/java/com/quostomize/quostomize_be/domain/customizer/cardapplication/repository/CardApplicantInfoRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/cardapplication/repository/CardApplicantInfoRepository.java
@@ -1,11 +1,18 @@
 package com.quostomize.quostomize_be.domain.customizer.cardapplication.repository;
 
+import com.quostomize.quostomize_be.domain.customizer.card.enums.CardStatus;
 import com.quostomize.quostomize_be.domain.customizer.cardapplication.entity.CardApplicantInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CardApplicantInfoRepository extends JpaRepository<CardApplicantInfo, Long> {
     Optional<CardApplicantInfo> findByCardApplicantInfoId(Long cardApplicationInfoId);
     Optional<CardApplicantInfo> findByResidenceNumber(String residenceNumber);
+    @Query("select c from CardApplicantInfo  c join c.cardDetail d where d.status = :status")
+    Page<CardApplicantInfo> findByCardStatus(@Param("status") CardStatus status, Pageable pageable);
 }
