105e4b81e00ca737bc6301c163a657f2c7dbc457
seonmin5 <seonmin5555@gmail.com>
2024-12-02 01:55:28 +0900

    Refactor/Admin: LessSearchStrategy LessSearchStrategy 기준금액 이하 검색 전략

---
+++ src/main/java/com/quostomize/quostomize_be/domain/customizer/payment/strategy/LessSearchStrategy.java
@@ -0,0 +1,15 @@
+package com.quostomize.quostomize_be.domain.customizer.payment.strategy;
+
+import com.quostomize.quostomize_be.domain.customizer.payment.entity.PaymentRecord;
+import com.quostomize.quostomize_be.domain.customizer.payment.service.PaymentRecordService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LessSearchStrategy implements PaymentSearchStrategy{
+    @Override
+    public Page<PaymentRecord> search(PaymentRecordService service, Pageable pageable, Long searchAmount) {
+        return service.getLessRecords(pageable, searchAmount);
+    }
+}
