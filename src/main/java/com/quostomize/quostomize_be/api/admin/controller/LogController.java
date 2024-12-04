508370437567d5585c246f2b23bbb0ad88ec52e9
ChanUi <hcu55@naver.com>
2024-12-04 15:48:49 +0900

    Feat/log: LogController controller 변경, 페이징 처리

--- src/main/java/com/quostomize/quostomize_be/api/admin/controller/LogController.java
+++ src/main/java/com/quostomize/quostomize_be/api/admin/controller/LogController.java
@@ -2,6 +2,7 @@ package com.quostomize.quostomize_be.api.admin.controller;
 
 import com.quostomize.quostomize_be.api.admin.dto.SystemLogResponseDto;
 import com.quostomize.quostomize_be.domain.log.entity.SystemLog;
+import com.quostomize.quostomize_be.domain.log.enums.LogType;
 import com.quostomize.quostomize_be.domain.log.repository.SystemLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/api/admin/logs")
 @RequiredArgsConstructor
@@ -21,15 +24,17 @@ public class LogController {
 
     private final SystemLogRepository systemLogRepository;
 
-    @GetMapping
-    public ResponseEntity<Page<SystemLogResponseDto>> getAllLogs(
+    @GetMapping("/login-logout")
+    public ResponseEntity<Page<SystemLogResponseDto>> getLoginLogoutLogs(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "20") int size,
             @RequestParam(defaultValue = "createdAt") String sortBy,
             @RequestParam(defaultValue = "DESC") Sort.Direction direction
     ) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortBy));
-        Page<SystemLog> logPage = systemLogRepository.findAll(pageable);
+        Page<SystemLog> logPage = systemLogRepository.findByLogTypeIn(
+                List.of(LogType.LOGIN_ATTEMPT, LogType.LOGIN_SUCCESS, LogType.LOGIN_FAILURE, LogType.LOGOUT), pageable
+        );
 
         Page<SystemLogResponseDto> responseDtoPage = logPage.map(SystemLogResponseDto::fromEntity);
 
