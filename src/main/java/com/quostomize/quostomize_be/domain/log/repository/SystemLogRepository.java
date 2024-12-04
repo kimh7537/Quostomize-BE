a5470ca47238a2ec4ee44d4a6f0f312432b461f3
ChanUi <hcu55@naver.com>
2024-12-04 15:48:24 +0900

    Feat/log: SystemLogRepository repository 추가, 페이징 처리

--- src/main/java/com/quostomize/quostomize_be/domain/log/repository/SystemLogRepository.java
+++ src/main/java/com/quostomize/quostomize_be/domain/log/repository/SystemLogRepository.java
@@ -2,6 +2,12 @@ package com.quostomize.quostomize_be.domain.log.repository;
 
 import com.quostomize.quostomize_be.domain.log.entity.SystemLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.quostomize.quostomize_be.domain.log.enums.LogType;
+
+import java.util.List;
 
 public interface SystemLogRepository extends JpaRepository<SystemLog, Long> {
+    Page<SystemLog> findByLogTypeIn(List<LogType> logTypes, Pageable pageable);
 }
