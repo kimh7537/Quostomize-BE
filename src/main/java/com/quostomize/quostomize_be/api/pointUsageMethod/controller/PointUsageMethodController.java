1fda8596f7f9b67e1bcda8816d410d9130896f70
kimh7537 <kimh7537@gmail.com>
2024-12-06 10:23:17 +0900

    Refactor/pointUsageMethod: 포인트 사용 주석 제거

--- src/main/java/com/quostomize/quostomize_be/api/pointUsageMethod/controller/PointUsageMethodController.java
+++ src/main/java/com/quostomize/quostomize_be/api/pointUsageMethod/controller/PointUsageMethodController.java
@@ -57,26 +57,4 @@ public class PointUsageMethodController {
         return ResponseEntity.noContent().build();
     }
 
-//    @PostMapping(value = "/{cardSequenceId}")
-//    @Operation(summary = "포인트 사용 옵션 변경", description = "포인트 사용 옵션 활성 상태를 변경합니다.(on/off)")
-//    public ResponseEntity<ResponseDTO> togglePointUsage(
-//            @PathVariable Long cardSequenceId,
-//            @RequestBody PointUsageMethodRequestDto pointUsageMethodRequestDto
-//    ) {
-//        Boolean isActive = pointUsageMethodRequestDto.isActive();
-//        String usageType = pointUsageMethodRequestDto.usageType();
-//
-//        PointUsageMethod updatedPointUsageMethod = pointUsageMethodService.togglePointUsage(
-//                cardSequenceId, usageType, isActive);
-//        PointUsageMethodResponseDto responseDto = PointUsageMethodResponseDto.from(updatedPointUsageMethod);
-//        ResponseDTO<PointUsageMethodResponseDto> response = new ResponseDTO(responseDto);
-//        return ResponseEntity.ok(response);
-//    }
-
-//    @PostMapping("/change-lotto")
-//    @Operation(summary = "로또 참여 설정 변경", description = "나의 카드페이지에서 로또 참여 여부를 ON/OFF 시 참여자 수가 변경됩니다.")
-//    public ResponseEntity<String> toggleLottoParticipation(@Valid @RequestBody LottoParticipantRequestDto request) {
-//        lottoService.toggleLottoParticipation(request);
-//        return ResponseEntity.ok("로또 참여 설정이 변경되었습니다.");
-//    }
 }
